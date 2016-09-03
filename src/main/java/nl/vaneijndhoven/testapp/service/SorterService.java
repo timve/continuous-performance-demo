bb78573db0f37f3c7db27ddfdc970a55b37d9629
Bert Jan Schrijver <bertjan@revolution.nl>
2016-09-03 21:55:40 +0200

    Minor changes.

--- src/main/java/nl/vaneijndhoven/testapp/service/SorterService.java
+++ src/main/java/nl/vaneijndhoven/testapp/service/SorterService.java
@@ -4,7 +4,7 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.rxjava.core.AbstractVerticle;
 import io.vertx.rxjava.core.eventbus.Message;
 import nl.vaneijndhoven.testapp.sorter.Sorter;
-import nl.vaneijndhoven.testapp.sorter.impl.InsertionSorter;
+import nl.vaneijndhoven.testapp.sorter.impl.BubbleSorter;
 
 import java.util.List;
 
@@ -12,8 +12,8 @@ public class SorterService extends AbstractVerticle {
 
     public static final String ADDRESS = "SorterService";
 
-    // bubble -> selection -> insertion
-    private Sorter sorter = new InsertionSorter();
+    // Supported implementations: Bubble Sort, Selection Sort, Insertion Sort.
+    private Sorter sorter = new BubbleSorter();
 
     @Override
     public void start() {
