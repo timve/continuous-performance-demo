673f194dc38a18fef6ed0bb99782ec1d180c3de6
Bert Jan Schrijver <bertjan@revolution.nl>
2018-11-20 20:38:24 +0100

    re-record HAR, re-generate reference simulation

--- reference/SorterAppSimulation.scala
+++ reference/SorterAppSimulation.scala
@@ -1,28 +1,28 @@
+package simulations
+
+import scala.concurrent.duration._
+
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
 
 class SorterAppSimulation extends Simulation {
 
 	val httpProtocol = http
-		.baseURL("http://localhost:8080")
+		.baseUrl("http://localhost:8080")
 		.inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.(t|o)tf""", """.*\.png""", """.*\.svg"""), WhiteList("""(.*)localhost(.+)"""))
+		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("nl,en;q=0.7,en-US;q=0.3")
+		.upgradeInsecureRequestsHeader("1")
+		.userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:63.0) Gecko/20100101 Firefox/63.0")
 
-	val headers_0 = Map(
-		"Cache-Control" -> "max-age=0",
-		"Upgrade-Insecure-Requests" -> "1")
-
-	val headers_1 = Map("Upgrade-Insecure-Requests" -> "1")
-
-    val uri1 = "http://localhost:8080"
+	val headers_0 = Map("Pragma" -> "no-cache")
 
 	val scn = scenario("SorterAppSimulation")
 		.exec(http("request_0")
-			.get("/")
-			.headers(headers_0))
-		.pause(9)
-		.exec(http("request_1")
 			.get("/?count=15000")
-			.headers(headers_1))
+			.headers(headers_0))
 
-	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+	setUp(scn.inject(rampUsers(300) during (10 seconds))).protocols(httpProtocol)
 }
